66cf5424b9715889a6bf8bffe0c30c04bd2dba19
LeKSuS-04 <tarasovion2004@gmail.com>
2021-07-28 12:10:35 +0300

    New structure and more functionality
    Restructured using object-oriented programming principles

--- README.md
+++ README.md
@@ -1,16 +1,75 @@
 # Cool logs
  Simple output beautifier for python 3
 
-## Usage
-There are 7 different modes:
-* Critical - for VERY important messages (red background and default foreground)
+## Basic usage:
+To use coollogs, you need to initialise Logger object. After that you can use it as you want!
+```
+import coollogs
+log = coollogs.Logger()
+log.success('Hello, logs!')
+```  
+The output will be as follows:  
+![Examples](screenshots/basic_usage_output.png)
+
+## Logging modes
+When logging, each message will be prefixed with current time (hh:mm:ss) and it's type. There are 10 built-in logging modes:
+* Critical - for VERY important messages (red background)
 * Error - for errors and stuff (red foreground)
 * Warning - you guessed it: for warnings (orange foreground)
-* Log - for defaulft logging (blue foreground)
-* New - when you discovered something new (green foreground)
-* Success - for bragging about success (green foreground)  
-* Custom - Set it how you like (custom type and color)
-
-Each message will be prefixed with time when it was sent (hh:mm:ss) and it's type. Keep in mind that colors might differ on different systems or might not work at all.
+* Info - for defaulft logging (cyan foreground)
+* Debug - for debugging purposes (purple foreground)
+* Plus - when you discovered something new (green foreground)
+* Minus - when you lose something important (red foreground)
+* Success - for bragging about the success (green foreground)  
+* Failure - to get upset about the failure  (red foreground)  
+* Custom - Set it how you like (custom type and color, explained later)  
 
 ![Examples](screenshots/logs_example.png)
+
+## Advanced: Logger object
+When creating logger object you can pass 3 different parameters to constructor:
+```
+log = coollogs.Logger(colorful=True, infolength=9, timecolor=coollogs.Colors.fg.CYAN)
+```
+| Parameter  |       Type      | Purpose                                                                                                                                             | Default value           |
+|------------|:---------------:|-----------------------------------------------------------------------------------------------------------------------------------------------------|:-----------------------:|
+|  colorful  |       bool      | Turns on/off color stylizing of the output                                                                                                          |           True          |
+|  loglevel  |       int       | Sets log level for the logger                                                                                                                       |            31           |
+| infolength |       int       | Determines amount of space for information block.  If information is longer than *infolength*, it'll be cut; otherwise it'll be padded with spaces. |            9            |
+|  timecolor | coollogs.Colors | Sets color for time                                                                                                                                 | coollogs.Colors.fg.CYAN |
+
+## Advanced: Custom logs
+If provided log modes don't satisfy you, you can create your own, using .custom():
+```
+log.custom('Hello, custom!', infoname='', infocolor=Colors.fg.CYAN)
+```
+| Parameter |       Type      | Purpose                                                                                                         | Default value           |
+|-----------|:---------------:|-----------------------------------------------------------------------------------------------------------------|:-----------------------:|
+|  infoname |       str       | Sets text in custom information block                                                                           |            ""           |
+| infocolor | coollogs.Colors | Sets color of custom information block. Can be a sum of different coollogs.Colors values to create mixed styles | coollogs.Colors.fg.CYAN |
+
+## Advanced: Logging level
+Even though there are 10 built-in logging modes, only 5 of them are "basic": debug, info, warning, error and critical. You can set logging level by using .set_level(int) on your logger object, for example:
+```
+log.set_level(7)
+```
+This will set logging level to a 7, which means that only critical messages, errors and warnings will be displayed. THe value passed into set_level() function is calulated by summing up every log level, which you want to be showed.
+Values for each level are as follows:
+| Level    | Value |
+|----------|:-----:|
+| CRITICAL |   1   |
+|   ERROR  |   2   |
+|  WARNING |   4   |
+|   INFO   |   8   |
+|   DEBUG  |   16  |
+
+So, for example, to show everything except debug messages, you have to set logging level to 1 + 2 + 4 + 8 = 15.  
+You don't have to memorize all the values, there are constants in LogLevels class to help you with it:
+```
+coollogs.LogLevels.CRITICAL = 1
+coollogs.LogLevels.ERROR    = 2
+coollogs.LogLevels.WARNING  = 4
+coollogs.LogLevels.INFO     = 8
+coollogs.LogLevels.DEBUG    = 16
+coollogs.LogLevels.ALL      = 31
+```
--- coollogs.py
+++ coollogs.py
@@ -1,97 +1,148 @@
 import time
 
-class colors:
-    reset='\033[0m'
-    bold='\033[01m'
-    disable='\033[02m'
-    underline='\033[04m'
-    reverse='\033[07m'
-    strikethrough='\033[09m'
-    invisible='\033[08m'
+class Colors:
+    class style:
+        RESET         = '\033[0m'
+        BOLD          = '\033[01m'
+        DISABLE       = '\033[02m'
+        UNDERLINE     = '\033[04m'
+        REVERSE       = '\033[07m'
+        STRIKETHROUGH = '\033[09m'
+        INVISIBLE     = '\033[08m'
     class fg:
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
+        BLACK         = '\033[30m'
+        RED           = '\033[31m'
+        GREEN         = '\033[32m'
+        ORANGE        = '\033[33m'
+        BLUE          = '\033[34m'
+        PURPLE        = '\033[35m'
+        CYAN          = '\033[36m'
+        LIGHTGRAY     = '\033[37m'
+        DARKGRAY      = '\033[90m'
+        LIGHTRED      = '\033[91m'
+        LIGHTGREEN    = '\033[92m'
+        YELLOW        = '\033[93m'
+        LIGHTBLUE     = '\033[94m'
+        PINK          = '\033[95m'
+        LIGHTCYAN     = '\033[96m'
     class bg:
-        black='\033[40m'
-        red='\033[41m'
-        green='\033[42m'
-        orange='\033[43m'
-        blue='\033[44m'
-        purple='\033[45m'
-        cyan='\033[46m'
-        lightgrey='\033[47m'
-
-def get_time():
-    return f'[{time.strftime("%H:%M:%S", time.localtime())}]'
-
-def get_type(type):
-    length = 9
-    if len(type) >= length:
-        return f'[{type[:length]}]'
-    else:
-        prefix = ' ' * ((length + 1- len(type)) // 2)
-        postfix = ' ' * ((length - len(type)) // 2)
-        return f'[{prefix}{type}{postfix}]'
+        BLACK         = '\033[40m'
+        RED           = '\033[41m'
+        GREEN         = '\033[42m'
+        ORANGE        = '\033[43m'
+        BLUE          = '\033[44m'
+        PURPLE        = '\033[45m'
+        CYAN          = '\033[46m'
+        LIGHTGRAY     = '\033[47m'
 
 
-def custom(*data, type = '', typecolor: colors = colors.fg.lightblue):
-    time = f'{colors.fg.cyan}{colors.bold}{get_time()}{colors.reset}'
-    type = f'{typecolor}{colors.bold}{get_type(type)}{colors.reset}'
-    text = f'{" ".join([str(x) for x in data])}'
-    print(f'{time} {type} {text}')
+class LogLevels():
+    CRITICAL = 1
+    ERROR    = 2
+    WARNING  = 4
+    INFO     = 8
+    DEBUG    = 16
+    ALL      = 31
+
+class Logger():
+    def __init__(self, colorful: bool = True, loglevel: int = LogLevels.ALL, infolength: int = 9, timecolor: Colors = Colors.fg.CYAN):
+        self._colorful = colorful
+        self._infolength = infolength
+        self._timecolor = timecolor
+        self._loglevel = loglevel
+
+
+    def _get_time(self):
+        return '[' + time.strftime("%H:%M:%S", time.localtime()) + ']'
+
+    def _get_info(self, infoname):
+        length = self._infolength
+
+        if len(infoname) >= length:
+            prefix = '['
+            postfix = ']'
+            infoname = infoname[:length]
+        else:
+            prefix = '[' + ' ' * ((length + 1- len(infoname)) // 2)
+            postfix = ' ' * ((length - len(infoname)) // 2) + ']'
+
+        return prefix + infoname + postfix
+
+
+    def set_level(self, loglevel: int):
+        self._loglevel = loglevel
+
+
+    def custom(self, *data, infoname='', infocolor: Colors = Colors.fg.CYAN):
+        if self._colorful:
+            time_color = self._timecolor + Colors.style.BOLD
+            info_color = infocolor + Colors.style.BOLD
+            reset = Colors.style.RESET
+        else:
+            time_color = info_color = reset = ''
+
+
+        time = time_color + self._get_time() + reset
+
+        info = info_color + self._get_info(infoname) + reset
+        text = f'{" ".join([str(x) for x in data])}'
+
+        print(f'{time} {info} {text}')
+        
+
+    def critical(self, *data):
+        if self._loglevel & (1 << 0):
+            self.custom(*data, infoname='CRITICAL', infocolor=Colors.bg.RED)
+
+    def error(self, *data):
+        if self._loglevel & (1 << 1):
+            self.custom(*data, infoname='ERROR', infocolor=Colors.fg.RED)
+
+    def warning(self, *data):
+        if self._loglevel & (1 << 2):
+            self.custom(*data, infoname='WARNING', infocolor=Colors.fg.ORANGE)
+
+    def info(self, *data):
+        if self._loglevel & (1 << 3):
+            self.custom(*data, infoname='INFO', infocolor=Colors.fg.CYAN)
+
+    def debug(self, *data):
+        if self._loglevel & (1 << 4):
+            self.custom(*data, infoname='DEBUG', infocolor=Colors.fg.PURPLE)
+
+
+    def plus(self, *data):
+        self.custom(*data, infoname='+', infocolor=Colors.fg.GREEN)
+
+    def minus(self, *data):
+        self.custom(*data, infoname='-', infocolor=Colors.fg.RED)
+
     
-def critical(*data):
-    custom(*data, type='CRITICAL', typecolor=colors.bg.red)
+    def success(self, *data):
+        self.custom(*data, infoname='SUCCESS', infocolor=Colors.fg.GREEN)
 
-def error(*data):
-    custom(*data, type='ERROR', typecolor=colors.fg.red)
-
-def warning(*data):
-    custom(*data, type='WARNING', typecolor=colors.fg.orange)
-
-def log(data = ''):
-    info(data)
-
-def info(*data):
-    custom(*data, type='INFO', typecolor=colors.fg.lightblue)
-
-def success(*data):
-    custom(*data, type='SUCCESS', typecolor=colors.fg.green)
-
-def debug(*data):
-    custom(*data, type='DEBUG', typecolor=colors.fg.purple)
+    def failure(self, *data):
+        self.custom(*data, infoname='FAILURE', infocolor=Colors.fg.RED)
 
 
-def plus(*data):
-    custom(*data, type='+', typecolor=colors.fg.green)
 
-def minus(*data):
-    custom(*data, type='-', typecolor=colors.fg.red)
-
-
-def demo():
-    critical('This is .critical()')
-    error('This is .error()')
-    warning('This is .warning()')
-    info('This is .info()')
-    success('This is .success()')
-    debug('This is .debug()')
-    print()
-    plus('This is .plus()')
-    minus('This is .minus()')
+    def demo(self):
+        self.critical('This is .critical()')
+        self.error('This is .error()')
+        self.warning('This is .warning()')
+        self.info('This is .info()')
+        self.debug('This is .debug()')
+        print()
+        self.plus('This is .plus()')
+        self.minus('This is .minus()')
+        print()
+        self.success('This is .success()')
+        self.failure('This is .failure()')
+        print()
+        self.custom('This is custom one, with infocolor = Colors.fg.ORANGE', infoname='CUSTOM #1', infocolor=Colors.fg.ORANGE)
+        self.custom('This is custom one, with infocolor = Colors.bg.PURPLE', infoname='CUSTOM #2', infocolor=Colors.bg.PURPLE)
+        self.custom('This is custom one, with infocolor = Colors.fg.BLACK + Colors.bg.ORANGE', infoname='CUSTOM #3', infocolor=Colors.fg.BLACK + Colors.bg.ORANGE)
 
 if __name__ == '__main__':
-    demo()
+    log = Logger()
+    log.demo()
